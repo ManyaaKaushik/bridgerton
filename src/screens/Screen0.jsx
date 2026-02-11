70df28372e99241aea30ff35b88d1e73ac2b5949
Manya Kaushik <78549658+ManyaaKaushik@users.noreply.github.com>
2026-02-11 09:26:20 +0000

    bg changed, spacing changed

--- src/screens/Screen0.jsx
+++ src/screens/Screen0.jsx
@@ -3,27 +3,27 @@ import React from "react";
 const Screen0 = ({ onStartClick }) => {
   return (
     <div
-      className="min-h-screen bg-[#461A00] flex flex-col justify-center items-cente relative overflow-hidden font-crimson bg-cover bg-center bg-no-repeat"
+      className="min-h-screen bg-[#ffffff] flex flex-col justify-center items-cente relative overflow-hidden font-crimson bg-cover bg-center bg-no-repeat"
       style={{
-        backgroundImage: "url('/bg.png')",
+        backgroundImage: "url('/bg1.png')",
         backgroundAttachment: "fixed",
         backgroundSize: "cover",
       }}
     >
       <div className="text-center z-10 animate-fade-in">
-        <h1 className="text-7xl font-luxurious text-dark-brown mb-2 tracking-tight drop-shadow-md">
+        <h1 className="text-8xl font-luxurious text-dark-brown mb-1 tracking-tight drop-shadow-md">
           Dearest &nbsp; Valentine
         </h1>
-        <p className="text-3xl italic tracking-tight font-linden-hill text-medium-brown mb-16 font-light">
+        <p className="text-4xl italic tracking-tight font-linden-hill text-medium-brown mb-8 font-light">
           A Bridgerton Affair
         </p>
 
         <a
-          className="relative w-64 h-36 mx-auto bg-cover bg-center bg-no-repeat hover:scale-105 transition-transform duration-200 cursor-pointer flex items-center justify-center"
+          className="relative scale-75 w-64 h-36 mx-auto bg-cover bg-center bg-no-repeat hover:scale-85 transition-transform duration-200 cursor-pointer flex items-center justify-center"
           style={{ backgroundImage: "url('/button-bg.png')" }}
           onClick={onStartClick}
         >
-          <span className="font-metal text-[40px] text-black leading-normal">
+          <span className="font-linden-hill text-[36px] text-dark-brown leading-normal">
             Get Started
           </span>
         </a>
